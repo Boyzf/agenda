fc57b188baa6ba414882379aef93c4962ea09aab
Boyzf <541153850@qq.com>
2019-12-03 15:01:35 +0800

    add

---
+++ cmd/logout.go
@@ -0,0 +1,66 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// logoutCmd represents the logout command
+var logoutCmd = &cobra.Command{
+	Use:   "logout",
+	Short: "Logout",
+	Long: `Logout the agenda.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		f, _ := os.OpenFile("entity/currentUser.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC,0644)
+
+		defer f.Close()
+		data:=[]byte("")
+		f.Write(data)
+
+		fmt.Println("Logout succeed!")
+	},
+}
+
+func init() {
+	fileptr,e :=  os.Open("entity/User.json")
+	defer fileptr.Close()
+	if e != nil && fileptr != nil{
+		fmt.Println(e)
+	}
+	if fileptr==nil{
+		fileptr, e := os.Create("entity/User.json")
+		defer fileptr.Close()
+		if e != nil{
+			fmt.Println(e)
+		}
+	}
+
+	RootCmd.AddCommand(logoutCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// logoutCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// logoutCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
